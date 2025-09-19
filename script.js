8d9aec2d354b9f815d5adaf2b7893a0d817d7c6c
inebw <punit965@gmail.com>
2025-09-20 02:46:04 +0530

    Calculation for brackets should take priority
    - Added logic for calculating the expression
    under brackets first

--- script.js
+++ script.js
@@ -3,10 +3,43 @@ const clearButton = document.querySelector('.clear-button');
 const userInput = document.querySelector('.user-input');
 const answer = document.querySelector('.answer')
 
+
+function removeBrackets(str) {
+    if (str.length == 0) return "";
+
+    let res = "";
+    let i = 0;
+    while (i < str.length ) {
+        if (str[i] == '(') {
+            let j = i + 1;
+            let op = 0;
+            while (str[j] != ')' && op == 0) {
+                if (str[j] == '(') op++;
+                if (str[j] == ')') op--;
+                j++;
+            }
+            let temp = returnCalculation(str.slice(i+1, j));
+            res += temp;
+            i = j;
+        }
+        else res += str[i]
+        i++;
+    }
+    return res
+}
+
 function returnCalculation(str) {
-    let numbers = str.split(/[+-/*]+/);
+    // console.log("here" + str)
+    str = str.replaceAll('x', '*');
+    str = str.replaceAll(' ', '');
+    if (str.includes('(')) {
+        str = removeBrackets(str);
+    }
+    console.log(str)
+    let numbers = str.split(/[+\-/*]+/);
+    console.log(numbers)
     let result = 0;
-    numbers = numbers.map(item => parseInt(item));
+    numbers = numbers.map(item => parseFloat(item));
     for (i = 0; i < str.length; i++) {
         if (str[i] == '+') {
             let a = numbers.shift();
@@ -14,14 +47,24 @@ function returnCalculation(str) {
             let c = a + b;
             numbers.unshift(c);
         } else if (str[i] == '-') {
-            let a;
+            let a = numbers.shift();
+            let b = numbers.shift();
+            let c = a - b;
+            numbers.unshift(c);
         } else if (str[i] == '/') {
-            let a;
+            let a = numbers.shift();
+            let b = numbers.shift();
+            let c = a / b;
+            numbers.unshift(c);
         } else if (str[i] == '*' || str[i] == 'x') {
-            let a;
+            let a = numbers.shift();
+            let b = numbers.shift();
+            let c = a * b;
+            numbers.unshift(c);
         } 
     }
     result = numbers.shift()
+    console.log(result)
     return result
 }
 
@@ -35,4 +78,17 @@ equalTo.addEventListener('click', () => {
 
 clearButton.addEventListener('click', () => {
     userInput.value = "";
+    answer.textContent = "Result: "
+})
+
+const allButtons = document.querySelectorAll('button');
+
+allButtons.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        if (elem.textContent !== '=' && elem.textContent !== 'C'){
+            userInput.value += elem.textContent;
+            answer.textContent = "Result: ";
+        }
+        
+    })
 })
